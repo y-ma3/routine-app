7be85c6c0d63c69d6d040e2af68cb08ab5327e79
Tsuri Yuma <yuma33t@gmail.com>
2024-05-20 16:22:22 +0900

    feat: 絵文字アイコンの選択を実装

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -4,26 +4,26 @@ import 'package:routine_app/view/input_form/input_form_view.dart';
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-                title: const Text('ホーム画面'),
-                backgroundColor: Colors.black12,
-            ),
-            body: const Center(
-                child: Text('ホーム画面'),
-            ),
-            floatingActionButton: FloatingActionButton(
-                onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const InputForm()),
-                    );
-                },
-                child: const Icon(Icons.add),
-            ),
-            floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ホーム画面'),
+        backgroundColor: Colors.black12,
+      ),
+      body: const Center(
+        child: Text('ホーム画面'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const InputForm()),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
+  }
 }
--- lib/view/input_form/input_form_view.dart
+++ lib/view/input_form/input_form_view.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:routine_app/component/text_field.dart';
+import 'package:emoji_selector/emoji_selector.dart';
 
-class InputForm extends StatelessWidget {
+class InputForm extends StatefulWidget {
   const InputForm({Key? key}) : super(key: key);
+
+  @override
+  InputFormState createState() => InputFormState();
+}
+
+class InputFormState extends State<InputForm> {
+  String selectedEmoji = "✅";
+  EmojiData? emojiData;
 
   @override
   Widget build(BuildContext context) {
@@ -12,12 +21,53 @@ class InputForm extends StatelessWidget {
         title: Text(AppLocalizations.of(context)!.inputFormTitle),
         backgroundColor: Colors.black12,
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           children: [
-            SizedBox(
-              width: 250,
-              child: MyTextField(),
+            const Padding(padding: EdgeInsets.all(10)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (BuildContext subcortex) {
+                          return SizedBox(
+                            child: EmojiSelector(
+                              padding: const EdgeInsets.all(20),
+                              onSelected: (emoji) {
+                                setState(() {
+                                  selectedEmoji = emoji.char;
+                                });
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          );
+                        },
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.all(10),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                      ),
+                      side: const BorderSide(color: Colors.black, width: 1),
+                    ),
+                    child: Text(
+                      selectedEmoji,
+                      style: const TextStyle(
+                        fontSize: 30,
+                      ),
+                    )),
+                const Padding(
+                  padding: EdgeInsets.all(10),
+                ),
+                const SizedBox(
+                  width: 250,
+                  child: MyTextField(),
+                ),
+              ],
             ),
           ],
         ),
