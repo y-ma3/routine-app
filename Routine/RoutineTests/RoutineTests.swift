823abd40d70c7fad63ec01c4ad72e2f9af9954fb
y-ma3 <yuma33t@gmail.com>
2024-10-17 20:42:10 +0900

    #3 環境構築
    プロジェクトを作成

---
+++ Routine/RoutineTests/RoutineTests.swift
@@ -0,0 +1,9 @@
+import Testing
+
+struct RoutineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
